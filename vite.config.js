b259780708beaf7d9f0746794334c290335c1759
Samarth01 <samarthsolanki56@gmail.com>
2024-12-10 19:33:27 +0530

    heyy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Password_Generator/', // Replace 'repository-name' with the name of your GitHub repository
+  plugins: [react()]
+ // Replace 'repository-name' with the name of your GitHub repository
 })
 
